8cd347f5ba5fc85e3ce0bfd48029862144986317
Sergio Avalos <sergioai.si93@gmail.com>
2025-06-04 23:40:26 -0500

    fix error styles and header

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,26 +1,29 @@
 'use strict'
 
 const header = document.getElementById("header-main");
-const parallaxSection = document.getElementById("parallax")
+const parallaxSection = document.getElementById("inicio")
 
 
 const btnOpen = document.querySelector(".toogle-nav-open");
 const btnClose = document.querySelector(".toogle-nav-close");
 const navBar = document.querySelector(".topnav-menu")
 
-window.addEventListener("scroll", () => {
-    const parallaxHeight = parallaxSection.offsetHeight;
+document.addEventListener("DOMContentLoaded", () => {
+    window.addEventListener("scroll", () => {
+        const parallaxHeight = parallaxSection.offsetHeight;
 
-    const scrolling = window.pageXOffset || document.documentElement.scrollTop;
+        const scrolling = window.pageXOffset || document.documentElement.scrollTop;
 
-    (scrolling > parallaxHeight * 0.9) ? header.classList.add("visible") : header.classList.remove("visible")
+        (scrolling > parallaxHeight * 0.9) ? header.classList.add("visible") : header.classList.remove("visible")
 
-})
+    })
 
-btnOpen.addEventListener("click", () => {
-    navBar.setAttribute("data-visible", "true");
-})
+    btnOpen.addEventListener("click", () => {
+        navBar.setAttribute("data-visible", "true");
+    })
 
-btnClose.addEventListener("click", () => {
-    navBar.setAttribute("data-visible", "false")
+    btnClose.addEventListener("click", () => {
+        navBar.setAttribute("data-visible", "false")
+    })
+
 })
